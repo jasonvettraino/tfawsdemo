bef94f6e327a887b9436d44d2de83e02631bc3c9
Jason Vettraino <Jason.Vettraino@Gmail.Com>
2021-07-13 17:23:32 +0100

    Add AWS Provider

--- tf/main.tf
+++ tf/main.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.37.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region  = var.region
+  profile = var.profile
+}
 
